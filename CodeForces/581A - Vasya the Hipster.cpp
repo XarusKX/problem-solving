237bab4a104b5959a5a4f23c15bca6ad55534b17
Anthony R T <belzeharkxarus@ymail.com>
2022-12-04 07:38:19 +0700

    Solved CF581A

--- CodeForces/581A - Vasya the Hipster.cpp
+++ CodeForces/581A - Vasya the Hipster.cpp
@@ -1,58 +1,12 @@
 #include <bits/stdc++.h>
-#include <stdlib.h>
 
 using namespace std;
 
-using vi = vector<int>;
-
-void fastscan(int &number) {
-    //variable to indicate sign of input number
-    bool negative = false;
-    register int c;
-
-    number = 0;
-
-    // extract current character from buffer
-    c = getchar();
-    if (c=='-')
-    {
-        // number is negative
-        negative = true;
-
-        // extract the next character from the buffer
-        c = getchar();
-    }
-
-    // Keep on extracting characters if they are integers
-    // i.e ASCII Value lies from '0'(48) to '9' (57)
-    for (; (c>47 && c<58); c=getchar())
-        number = number *10 + c - 48;
-
-    // if scanned input has a negative sign, negate the
-    // value of the input number
-    if (negative)
-        number *= -1;
-}
-
 int main () {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int a, b, c, d = 0;
+    scanf("%d %d", &a, &b);
 
-    int n, m;
-    cin >> n >> m;
-
-    int j = 0;
-    for (int i = 0; i < n; i++) {
-
-        for (; j < m; j++) {
-            cout << '#';
-        }
-
-        for (int j = 0; j < m; j++) {
-            cout << '.';
-        }
-        cout << endl;
-    }
-
-    return 0;
+    c = min(a, b);
+    d = (max(a, b) - min(a, b)) / 2;
+    printf("%d %d\n", c, d);
 }
