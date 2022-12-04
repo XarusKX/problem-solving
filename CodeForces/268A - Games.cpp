3eb5c43fc662ed1bb8b310144193ce9e28116684
Anthony R T <belzeharkxarus@ymail.com>
2022-12-04 07:28:17 +0700

    Solved CF268A

--- CodeForces/268A - Games.cpp
+++ CodeForces/268A - Games.cpp
@@ -1,50 +1,24 @@
 #include <bits/stdc++.h>
-#include <stdlib.h>
 
 using namespace std;
 
-using vi = vector<int>;
-
-void fastscan(int &number) {
-    //variable to indicate sign of input number
-    bool negative = false;
-    register int c;
-
-    number = 0;
-
-    // extract current character from buffer
-    c = getchar();
-    if (c=='-')
-    {
-        // number is negative
-        negative = true;
-
-        // extract the next character from the buffer
-        c = getchar();
-    }
-
-    // Keep on extracting characters if they are integers
-    // i.e ASCII Value lies from '0'(48) to '9' (57)
-    for (; (c>47 && c<58); c=getchar())
-        number = number *10 + c - 48;
-
-    // if scanned input has a negative sign, negate the
-    // value of the input number
-    if (negative)
-        number *= -1;
-}
-
 int main () {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
 
     int n;
-    cin >> n;
+    scanf("%d", &n);
 
-    int h, a;
-    while(n--) {
-        cin >> h >> a;
+    int team[n][2];
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &team[i][0], &team[i][1]);
     }
 
-    return 0;
+    int hostInGuest = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) continue;
+            hostInGuest += (team[i][0] == team[j][1]) ? 1 : 0;
+        }
+    }
+
+    printf("%d\n", hostInGuest);
 }
